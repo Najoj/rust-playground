43f8d6515c7702f930b46c8e2c34e4a4ae6b5337
Johan Öhlin <johanohlin@gmx.com>
2021-03-09 22:45:59 +0100

    a bit neater

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 use std::io::Read;
+use std::collections::HashSet;
 
 fn main() {
     let word: Vec<char> = "Secretmessage".chars().collect();
     let mut found: Vec<char> = "*".repeat(word.len()).chars().collect();
-    let mut guesses: u64 = 0;
+
+    let mut guessed = HashSet::new();
 
     while let Some(c) = std::io::stdin().bytes().next().and_then(|result| result.ok()).map(|byte| byte as char) {
-        if 'c' == '\n' {
-            guesses -= 1;
+        if c == '\n' {
+            continue;
+        } else if guessed.contains(&c) {
+            println!("Already guessed {}", c);
         } else {
-            guesses += 1;
+            guessed.insert(c);
             for i in 0..word.len() {
                 if word[i] == c {
                     found[i] = word[i];
                 }
             }
-
-            println!("{:?}", found);
-            if word == found {
-                break;
-            }
         }
 
+        println!("{:?}", found);
+
+        if word == found {
+            break;
+        }
     }
-    println!("Gussed letters {:?}", guesses)
+
+    println!("Done! Gussed {:?} letters.", guessed.len())
 }
